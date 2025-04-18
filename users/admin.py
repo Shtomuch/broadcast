f5bfa2897187dbf0e9e63e060f63379bedcc4510
Denys <denys.shtoma@knu.ua>
2025-04-18 14:45:11 +0300

    Implement user management features with API and admin integration

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ('Додаткові поля', {'fields': ('department',)}),
+    )
+    list_display = ('username', 'email', 'first_name', 'last_name', 'department', 'is_staff')
